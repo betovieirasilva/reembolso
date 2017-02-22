6dea0b608672ff160f17dcc1ab2de1fe57ea6f7f
Gilberto Vieira da Silva <gilberto.silva@tecsinapse.com.br>
2017-02-22 18:00:26 -0300

    Migrado o componente Reembolso para utilizar FormComponentBean. Desta forma já existe um cadastro pronto para ser utilizado

--- src/components/Reembolso.js
+++ src/components/Reembolso.js
@@ -4,60 +4,67 @@
 import React from 'react'
 
 import { Form } from 'react-bootstrap';
-import { FormGroup } from 'react-bootstrap';
 import { FormControl } from 'react-bootstrap';
+import { FormGroup } from 'react-bootstrap';
 import { ControlLabel } from 'react-bootstrap';
 import { Col } from 'react-bootstrap';
 import { Checkbox } from 'react-bootstrap';
 import { ButtonToolbar } from 'react-bootstrap';
 import { Button } from 'react-bootstrap';
 
-const Reembolso = () => (
-    <Form horizontal>
-        <FormGroup controlId="custo">
-            <Col componentClass={ControlLabel} sm={2}>
-                <ControlLabel>Custo</ControlLabel>
-            </Col>
-            <Col sm={10}>
-                <FormControl type="text" placeholder="Custo" />
-            </Col>
-        </FormGroup>
-        <FormGroup controlId="data">
-            <Col componentClass={ControlLabel} sm={2}>
-                <ControlLabel>Data</ControlLabel>
-            </Col>
-            <Col sm={10}>
-                <FormControl type="text" placeholder="01/02/2017" />
-            </Col>
-        </FormGroup>
-        <FormGroup controlId="categoria">
-            <Col componentClass={ControlLabel} sm={2}>
-                <ControlLabel>Categoria</ControlLabel>
-            </Col>
-            <Col sm={10}>
-                <FormControl componentClass="select" placeholder="Categoria">
-                    <option value="select">A</option>
-                    <option value="other">...</option>
-                </FormControl>
-            </Col>
-        </FormGroup>
-        <FormGroup controlId="observacao">
-            <Col componentClass={ControlLabel} sm={2}>
-                <ControlLabel>Observação</ControlLabel>
-            </Col>
-            <Col sm={10}>
-                <FormControl componentClass="textarea" placeholder="Observação" />
-            </Col>
-        </FormGroup>
-        <FormGroup>
-            <Col smOffset={2} sm={10}>
-                <ButtonToolbar>
-                    <Button bsStyle="primary">Salvar</Button>
-                    <Button>Excluir</Button>
-                </ButtonToolbar>
-            </Col>
-        </FormGroup>
-    </Form>
-)
+import FormComponentBean from './FormComponentBean';
+import FormControlBean from './FormControlBean';
+
+class Reembolso extends FormComponentBean {
+    render() {
+        return (
+            <Form horizontal>
+                <FormGroup controlId="custo">
+                    <Col componentClass={ControlLabel} sm={2}>
+                        <ControlLabel>Custo</ControlLabel>
+                    </Col>
+                    <Col sm={10}>
+                        <FormControlBean name="custo" parent={this} />
+                    </Col>
+                </FormGroup>
+                <FormGroup controlId="data">
+                    <Col componentClass={ControlLabel} sm={2}>
+                        <ControlLabel>Data</ControlLabel>
+                    </Col>
+                    <Col sm={10}>
+                        <FormControlBean name="data" parent={this} />
+                    </Col>
+                </FormGroup>
+                <FormGroup controlId="categoria">
+                    <Col componentClass={ControlLabel} sm={2}>
+                        <ControlLabel>Categoria</ControlLabel>
+                    </Col>
+                    <Col sm={10}>
+                        <FormControl componentClass="select" placeholder="Categoria">
+                            <option value="select">A</option>
+                            <option value="other">...</option>
+                        </FormControl>
+                    </Col>
+                </FormGroup>
+                <FormGroup controlId="observacao">
+                    <Col componentClass={ControlLabel} sm={2}>
+                        <ControlLabel>Observação</ControlLabel>
+                    </Col>
+                    <Col sm={10}>
+                        <FormControlBean componentClass="textarea" name="observacao" parent={this} />
+                    </Col>
+                </FormGroup>
+                <FormGroup>
+                    <Col smOffset={2} sm={10}>
+                        <ButtonToolbar>
+                            <Button bsStyle="primary" onClick={this.save.bind(this)}>Salvar</Button>
+                            <Button>Excluir</Button>
+                        </ButtonToolbar>
+                    </Col>
+                </FormGroup>
+            </Form>
+        )
+    }
+}
 
 export default Reembolso
